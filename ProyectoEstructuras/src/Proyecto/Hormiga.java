ef94bab7bfdfed8b8099baf41817045a7cd35abd
Yoarly Castillo M <82921283+yoita5@users.noreply.github.com>
2024-02-25 13:29:45 -0400

    Entrega Final 🔥🔥

---
+++ ProyectoEstructuras/src/Proyecto/Hormiga.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Proyecto;
+
+/**
+ *
+ * @author yoita5
+ */
+public class Hormiga {
+    double distancia;
+    String recorrido;
+    
+    public Hormiga(){
+        this.distancia = 0;
+        recorrido = "";
+    }
+}
---
+++ ProyectoEstructuras/src/Proyecto/Lista.java
@@ -0,0 +1,106 @@
+
+package Proyecto;
+
+
+public class Lista{
+    Nodo primero;
+    public int tamano;
+    public Lista(){
+        this.tamano = 0;
+        this.primero = null;
+    }
+    public void AgregarCiudades(int num, double d){
+        Nodo ciudad = new Nodo(num, d);
+        Nodo aux = primero;
+        if(primero == null){
+            primero = ciudad;
+            tamano++;
+        }else{
+        while(aux.siguiente != null){
+            aux = aux.siguiente;
+        }
+        aux.siguiente = ciudad;
+        tamano ++;
+    }
+    }
+    public void EliminarCiudades(int num){
+        if(primero == null){
+            System.out.println("No hay Ciudades");
+        }
+        else if(primero.num == num){
+            primero = primero.siguiente;
+            tamano--;
+
+        }else{
+            Nodo aux = primero;
+            while(aux.siguiente != null && aux.siguiente.num != num ){
+                aux = aux.siguiente;                
+            }
+            if(aux.siguiente!= null){
+                aux.siguiente = aux.siguiente.siguiente;}
+        }
+        
+    }
+    public Nodo BuscarCiudades(int num){
+        Nodo aux = primero;
+        while(aux != null && aux.num != num){
+            aux = aux.siguiente;
+        }
+        return aux;
+        
+    }
+    public String ImprimirCiudades(){
+        Nodo aux = primero;
+        String lista  = "";
+        if(aux != null){
+             lista = "Las cuidades adyacentes a " + aux.num + " son: ";  
+            aux = aux.siguiente;
+            while(aux != null){   
+                if(aux.siguiente != null){
+                    lista += aux.num + ", ";}
+                else{
+                    lista += aux.num;
+                }
+                aux = aux.siguiente;
+
+            }}
+        return lista;
+    }
+    
+    public void BuscarMayorValorArista(){
+        Nodo aux = primero.siguiente;
+        Nodo valor = aux;
+        while(aux != null){
+            if(aux.feromonas > valor.feromonas){
+                valor = aux;
+            }
+            aux = aux.siguiente;
+        }
+        System.out.println(valor.feromonas);
+    }
+    
+    public void iniciarFeromonas(double nodo){
+        Nodo aux = this.primero;
+        if(this.primero != null){
+            aux = aux.siguiente;
+            while(aux!= null){
+                aux.feromonas = 1/nodo;
+                aux = aux.siguiente;
+            }
+        }
+    }
+    
+    public String verFeromonas(){
+        Nodo aux = this.primero;
+        String a = "";
+        if(this.primero != null){
+            aux = aux.siguiente;
+            while(aux!= null){
+                a+= aux.feromonas + ", ";
+                aux = aux.siguiente;
+            }
+        }
+        
+        return a;
+    }
+}
